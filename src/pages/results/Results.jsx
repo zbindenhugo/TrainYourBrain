73ddd540a3ee7272675a3fda2da65e75a3ff94e0
hugo.zbinden@autajon.com <Zbindenh_Autajon>
2022-12-05 17:29:08 +0100

    Ajout de la page des résultats

--- src/pages/results/Results.jsx
+++ src/pages/results/Results.jsx
@@ -1,27 +1,34 @@
 import { useEffect } from "react";
 
-export default function Results({results, questions}){
+export default function Results({results, questions, score}){
 
-    var score = 0;
+    const tabQuestAnsw = [];
 
-    const calcScore = () => {
-        for(let i = 0; i < questions.length; i++){
-            const j = i + 1;
-
-            if(results[j] === questions[i])
-                score += 1;
-        }
+    for(let i = 0; i < questions.length; i++){
+        tabQuestAnsw.push(
+            <ul className="text-left">
+                <p className="font-bold">{ questions[i].question }</p>
+                <li><span className="font-bold">Correct Answer :</span> {questions[i].correctAnswer}   
+                </li><span className="font-bold">Your answer :</span> {results[i]}
+            </ul>
+        )
     }
 
-    useEffect(() => {
-        calcScore();
-    }, [questions])
-
     return(
-        <div>
-            {
-                questions ? score : 'Waiting for results ...'
-            }
+        <div className="w-[100vw] h-[100vh] text-center">
+            <div className=" w-full p-10">
+                <div className="sm:text-5xl text-xl">
+                    {`It's finished ! You have a score of ${score}/${questions.length}`}
+                </div>
+                <div className="mt-10">
+                    Here's a resume of the questions and your answers :
+                    <div className="grid grid-cols-3 gap-5 mt-10">
+                        {
+                            tabQuestAnsw
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
